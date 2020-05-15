3d9b1a485553612c5921d8623b57978d57dd80a1
JuanFrood <juan_f.rodriguez_c@uao.edu.co>
2020-05-15 02:34:32 -0500

    Taller
    Correccion y organizacion de botones
    Inicio con el sistema de rayos para la posicon de las palmeras.
    Ajuste de colisiones de las palmeras.

---
+++ Assets/Scrip/Rayos.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rayos : MonoBehaviour
+{
+    [SerializeField]
+    public Camera cam;
+   
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+    //Check if mouse is clicked
+    if (Input.GetMouseButtonDown(0))
+    {
+        RaycastHit hit;
+
+        //Get ray from mouse postion
+        Ray rayCast = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //Raycast and check if any object is hit
+        if (Physics.Raycast(rayCast, out hit))
+        {
+            //Check which tag is hit
+            if (hit.collider.CompareTag("palmeras"))
+            {
+                print(hit.collider.transform.position);
+            }
+        }
+    }
+  }
+
+}
