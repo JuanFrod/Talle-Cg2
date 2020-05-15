a7973c43c12cd85e2465597a8244cc0b66e6b434
SantiagoRPRO <60673370+SantiagoRPRO@users.noreply.github.com>
2020-05-14 23:52:42 -0500

    climas
    la lluvia solo es poner el controlador de la lluvia dentro del boton que cambie a lluvioso

---
+++ Assets/Scrip/Clima.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clima : MonoBehaviour
+{
+   public ParticleSystem ps;
+    
+   
+
+   public void cambiarClima()
+    {
+       if(ps.isEmitting)
+        {
+            ps.Stop();
+          
+        }
+        else
+        {
+            ps.Play();
+          
+        }
+    }
+}
